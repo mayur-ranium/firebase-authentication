4d9cc2068525eb047c2e6cedd8ab570c53311e5a
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2022-04-18 16:53:58 +0530

    create env file for keys

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,7 @@
         <h1 class="text-3xl">Welcome Home!!!</h1>
      
 
-        <p>{{VITE_API_KEY}}</p>
-        <p>{{VITE_APP_ID}}</p>
-        <p>{{VITE_PROJECT_ID}}</p>
+       
 
 
     </div>
@@ -17,11 +15,7 @@ export default {
     name: "Home",
    data(){
        return{
-         
-           VITE_API_KEY: import.meta.env.VITE_API_KEY,
-           VITE_APP_ID: import.meta.env.VITE_APP_ID,
-           VITE_PROJECT_ID: import.meta.env.VITE_PROJECT_ID
-           
+        
      
 
 
