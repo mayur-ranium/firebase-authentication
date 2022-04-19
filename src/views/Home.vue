64ba22de69d18703cdbddc04a338053c28a45f4a
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2022-04-19 10:36:01 +0530

    changes in home file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,6 @@
         <h1 class="text-3xl">Welcome Home!!!</h1>
      
 
-        <p>{{VITE_API_KEY}}</p>
-        <p>{{VITE_APP_ID}}</p>
-        <p>{{VITE_PROJECT_ID}}</p>
 
 
     </div>
@@ -17,10 +14,7 @@ export default {
     name: "Home",
    data(){
        return{
-         
-           VITE_API_KEY: import.meta.env.VITE_API_KEY,
-           VITE_APP_ID: import.meta.env.VITE_APP_ID,
-           VITE_PROJECT_ID: import.meta.env.VITE_PROJECT_ID
+    
            
      
 
